ab052f5d99791f7155ce62c623992ea3f55bf297
Tamilnaducm <92597966+Tamilnaducm@users.noreply.github.com>
2021-11-29 23:21:56 +0530

    Update forcesub.py

--- helper_funcs/forcesub.py
+++ helper_funcs/forcesub.py
@@ -13,7 +13,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
+                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/groupdc).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -25,7 +25,7 @@ async def ForceSub(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/TeleRoidGroup")
+                        InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/groupdcbots")
                     ]
                 ]
             ),
@@ -35,7 +35,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/TeleRoid14).\n\n**@TheTeleRoid**",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/groupdcbots).\n\n**@groupdcbots**",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
